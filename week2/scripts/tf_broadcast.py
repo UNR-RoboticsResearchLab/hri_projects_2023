b009c3a156592bc3b49c7cf30bbaf99e9ad4a0e5
denielleoliva <denielleoliva@gmail.com>
2023-09-24 13:44:59 -0700

    shakes at the sight of legs and walls

--- week2/scripts/tf_broadcast.py
+++ week2/scripts/tf_broadcast.py
@@ -14,44 +14,81 @@ from math import atan2
 from nav_msgs.msg import Odometry
 
 msg_twist=Twist()
-case=''
 linearx=0
 angularz=0
 
-goal=Point()
 theta=0
 
-def new_odom(dat):
-    global x
-    global y
-    global theta
+isBlocked=True
+foundLegs=False
+odomData=None
 
-    x = dat.pose.pose.position.x
-    y = dat.pose.pose.position.y
+goal=Point()
+
+def localRad(data):
+    global odomData, theta
+    
+    odomData = data
+
+    local_or = data.pose.pose.orientation
+    or_list = [local_or.x, local_or.y, local_or.z, local_or.w]
+    theta = local_or.z
+
+def faceHuman(rad):
+    global theta, odomData
+
+    if(rad > odomData.pose.pose.orientation.z):
+        while(theta<rad):
+            msg_twist.linear.x=0
+            msg_twist.angular.z=0.5
+            pub.publish(msg_twist)
+            rospy.sleep(0.1)
+    else:
+        while(theta > rad):
+            msg_twist.linear.x=0
+            msg_twist.angular.z=-0.5
+            pub.publish(msg_twist)
+            rospy.sleep(0.1)
+
+
 
 def avoid_follow(dat):
+    global isBlocked, linearx, angularz, msg_twist
+
     range={
         "right" : min(min(dat.ranges[0:239]) , 2),
         "center" : min(min(dat.ranges[240:479]) , 2),
         "left" : min(min(dat.ranges[480:719]) , 2)
     }
 
-    if ( range["right"] > 1  and range["center"] < 1 and range["left"] > 1 ):
-        print("front wall")
+    if ( range["right"] >1  and range["center"] > 1 and range["left"] >1):
+        isBlocked=False
+        linearx=0.4
+        angularz=0
+        print("front free")
+    elif ( range["right"] > 1  and range["center"] < 1 and range["left"] > 1 ):
+        isBlocked=True
         linearx=0
         angularz=-0.5
+        print("front wall")
     elif ( range["right"] < 1  and range["center"] > 1 and range["left"] > 1 ):
-        print("right wall")
+        isBlocked=True
         linearx=0
         angularz=0.5
+        print("right wall")
     elif ( range["right"] > 1  and range["center"] > 1 and range["left"] < 1 ):
-        print("left wall")
+        isBlocked=True
         linearx=0
         angularz=-0.5
+        print("left wall")
 
+    msg_twist.linear.x=linearx
+    msg_twist.angular.z=angularz
+    pub.publish(msg_twist)
 
 
 def handle_leggies(msg):
+    global foundLegs, isBlocked, linearx, angularz, msg_twist
     br = tf2_ros.TransformBroadcaster()
     t = geometry_msgs.msg.TransformStamped()
 
@@ -61,53 +98,32 @@ def handle_leggies(msg):
 
     if len(msg.people) == 0:
         print("no leggies")
-
-        linearx=0.0
-        angularz=0.2
-
-        msg_twist.linear.x = linearx
-        msg_twist.angular.z = angularz
-
-        pub.publish(msg_twist)
-
+        foundLegs=False
         return None
 
     t.transform.translation.x = msg.people[0].pos.x
     t.transform.translation.y = msg.people[0].pos.y
-    t.transform.translation.z = 0.0
+    t.transform.translation.z = msg.people[0].pos.z
     q = tf_conversions.transformations.quaternion_from_euler(0, 0, math.pi/2)
     t.transform.rotation.x = q[0]
     t.transform.rotation.y = q[1]
     t.transform.rotation.z = q[2]
     t.transform.rotation.w = q[3]
-    theta=t.transform.rotation.z
 
     br.sendTransform(t)
 
-    print(f"leggies found:{t.transform.translation}")
+    print(f"leggies found:{t.transform.translation.z}")
     
-    goal.x = t.transform.rotation.x
-    goal.y = t.transform.rotation.y
+    faceHuman(t.transform.rotation.z)
 
-    print(goal.x)
-
-    linearx=0.5
-    angularz=0.0
-
-    avoid_follow
-
-    msg_twist.linear.x = linearx
-    msg_twist.angular.z = angularz
-
-    pub.publish(msg_twist)
 
 
 def listener():
     global pub
 
     rospy.init_node('leggies_broadcaster')
+    rospy.Subscriber('/odom', Odometry, localRad)
     rospy.Subscriber('/people_tracker_measurements', PositionMeasurementArray, handle_leggies)
-    
     rospy.Subscriber('/base_scan', LaserScan, avoid_follow)
     pub = rospy.Publisher("/cmd_vel" , Twist , queue_size=1)
     
