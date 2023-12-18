7487e212b87f4e1ad05d00044943a764890b77a1
denielleoliva <denielleoliva@gmail.com>
2023-12-18 10:03:59 -0800

    speech recognition - yes/no (no not working)

---
+++ weekA/scripts/speech_listen_respond.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# license removed for brevity
+
+import rospy
+import ros_vosk
+
+from std_msgs.msg import String
+
+message = ""
+
+count = 0
+
+yes_response = False
+
+pub = rospy.Publisher('tts/phrase', String, queue_size=10)
+
+def listen(msg):
+    global message
+    print("Listened to:" + msg.data)
+
+    message=msg.data
+
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('listen_repeat', anonymous=True)
+        sub = rospy.Subscriber('/speech_recognition/final_result', String, listen)
+        rate = rospy.Rate(10) # 10hz
+
+        pub.publish("is it raining today?")
+
+        is_raining = False
+
+        has_umbrella = False
+
+        rospy.sleep(1)
+        while(message!="yes" or message!='no'):
+            rospy.sleep(1)
+
+            if(message=="yes"):
+                is_raining=True
+                pub.publish("do you have an umbrella?")
+
+                if(message=="yes"):
+                    has_umbrella=True
+                    pub.publish("good! i hope you stay dry today")
+                    break
+            
+            elif(message=="no"):
+                is_raining==False
+                pub.publish("i hope it is a good day outside then")
+                rospy.sleep(1)
+
+            else:
+                print("waiting")
+        
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
